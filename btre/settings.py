e2f02f0f6b4315b735b28c0b7d87f8e9eee07b16
jalajrastogi4 <jalajrastogi4@gmail.com>
2019-08-06 16:47:53 +0530

    update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'OgaTatsumi1',
+        'PASSWORD': 'abc123!',
         'HOST': 'localhost'
     }
 }
